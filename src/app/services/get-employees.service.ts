1eaa131235ac25c71d2409ee9d28970d82f8eaac
jussaraalves <jussaraalves852@gmail.com>
2024-09-06 23:00:15 -0300

    feat: create a method to get all employees and create a new interface

--- src/app/services/get-employees.service.ts
+++ src/app/services/get-employees.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Employee } from '../../types/employee.interface';
+import { EmployeeResponse } from 'src/types/employee-response.interface';
+
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +12,7 @@ export class GetEmployeesService {
   readonly url : string = 'assets/data/employee.json';
   constructor(private httpClient: HttpClient) { }
 
-  getAllEmployees(): Observable<Employee> {
-    return this.httpClient.get<Employee>(this.url);
+  getAllEmployees(): Observable<EmployeeResponse> {
+    return this.httpClient.get<EmployeeResponse>(this.url);
   }
 }
